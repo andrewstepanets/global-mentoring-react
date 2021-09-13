ae1910143f0b1bd6c7b66783f24ba9402bce5ddf
Andrew Stepanets <andrii.stepanets@vertexinc.com>
2021-09-13 13:01:19 +0300

    add eslint, typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,33 @@
-const path = require("path");
-const dotenv = require("dotenv");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
 
 const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
 
 module.exports = {
-  mode: isDev ? "development" : "production",
-  entry: ["./index.js"],
+  mode: isDev ? 'development' : 'production',
+  performance: {
+    hints: false,
+  },
+  entry: ['./src/index.tsx'],
   output: {
-    filename: `./js/${filename("js")}`,
-    chunkFilename: "[name].bundle.js",
-    path: path.resolve(__dirname, "build"),
+    filename: `./js/${filename('js')}`,
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
   },
   resolve: {
-    modules: [path.resolve(__dirname, "./src"), "node_modules"],
-    extensions: [".js", ".jsx", ".json"],
+    modules: [path.resolve(__dirname, './src'), 'node_modules'],
+    extensions: ['.js', '.json', '.tsx', '.ts'],
+    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
   },
-  watch: true,
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
-    contentBase: path.join(__dirname, "build"),
+    contentBase: path.join(__dirname, 'build'),
     compress: true,
     port: 4000,
     open: true,
@@ -32,64 +35,69 @@ module.exports = {
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
   },
   plugins: [
-    new CleanWebpackPlugin(["build"]),
+    new CleanWebpackPlugin(['build']),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./index.html"),
-      filename: "index.html",
-      favicon: "./src/assets/favicon.ico",
+      template: path.resolve(__dirname, './index.html'),
+      filename: 'index.html',
+      favicon: './src/assets/favicon.ico',
       minify: {
         collapseWhitespace: isProd,
       },
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[name].css",
+      filename: '[name].css',
+      chunkFilename: '[name].css',
     }),
   ],
   resolveLoader: {
     alias: {
-      "remove-comments-loader": path.join(
+      'remove-comments-loader': path.join(
         __dirname,
-        "./src/loaders",
-        "remove-comments-loader.js"
+        './src/loaders',
+        'remove-comments-loader.js',
       ),
-      "add-easter-egg-loader": path.join(
+      'add-easter-egg-loader': path.join(
         __dirname,
-        "./src/loaders",
-        "add-easter-egg-loader.js"
+        './src/loaders',
+        'add-easter-egg-loader.js',
       ),
     },
   },
   module: {
     rules: [
       {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
       {
         test: /\.html$/,
         exclude: /node_modules/,
         use: [
           {
-            loader: "add-easter-egg-loader",
+            loader: 'add-easter-egg-loader',
             options: { attrs: false },
           },
           {
-            loader: "remove-comments-loader",
+            loader: 'remove-comments-loader',
             options: { attrs: false },
           },
           {
-            loader: "html-loader",
+            loader: 'html-loader',
             options: { attrs: false },
           },
         ],
@@ -97,9 +105,9 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               modules: true,
             },
@@ -108,16 +116,16 @@ module.exports = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(gif|png|jpe?g|svg|ico)$/i,
         use: [
           {
-            loader: "file-loader",
-            options: { name: "img/[name].[ext]" },
+            loader: 'file-loader',
+            options: { name: 'img/[name].[ext]' },
           },
-          "image-webpack-loader",
+          'image-webpack-loader',
         ],
       },
     ],
